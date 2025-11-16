32dc12d4efceaa3431a53f97722add09d51ee3f1
Higor.Malaquias <higormalaquias@outlook.com>
2025-11-15 21:00:12 -0300

    feat: new function 'calculaMedia'

---
+++ calculaMedia.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+float mediaNumeros(int vetor[], int tamanho)
+{
+    int soma = 0;
+
+    for (int i = 0; i < tamanho; i++)
+    {
+        soma += vetor[i];
+    }
+
+    return (float)soma / tamanho;
+}
+
+int main()
+{
+    int numeros[] = {-1, 4, -4, -7, 8, 9, -10, 2, 11, 10};
+    int tamanho = sizeof(numeros) / sizeof(numeros[0]);
+
+    float media = mediaNumeros(numeros, tamanho);
+
+    printf("A media dos numeros: %.2f\n", media);
+
+    return 0;
+}
